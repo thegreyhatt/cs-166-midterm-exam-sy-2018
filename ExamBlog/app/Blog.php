ddde5bbd9cc046f229667e5087c1f15e1180b553
Mecel Joy Mamac <Mamac-Me@ndmu.net>
2018-12-18 12:32:57 +0800

    commit 2

--- ExamBlog/app/Blog.php
+++ ExamBlog/app/Blog.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    protected $fillable = ['title', 'body', 'category', 'short_description'];
+    protected $fillable = ['title', 'description', 'wholesale_price', 'price', 'category'];
 }
